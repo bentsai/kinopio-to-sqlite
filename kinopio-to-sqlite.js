121f2a3b4a9e806f4b01ae9c3237169b0b3bea42
Ben Tsai <bentsai@gmail.com>
2021-03-14 23:57:41 -0400

    Add more tables

--- kinopio-to-sqlite.js
+++ kinopio-to-sqlite.js
@@ -36,6 +36,27 @@ const insertSpace = async (id) => {
           input: JSON.stringify(cards),
         }
       );
+      spawnSync(
+        "sqlite-utils",
+        ["insert", "kinopio.db", "connections", "-", "--pk=id"],
+        {
+          input: JSON.stringify(connections),
+        }
+      );
+      spawnSync(
+        "sqlite-utils",
+        ["insert", "kinopio.db", "connectionTypes", "-", "--pk=id"],
+        {
+          input: JSON.stringify(connectionTypes),
+        }
+      );
+      spawnSync(
+        "sqlite-utils",
+        ["insert", "kinopio.db", "tags", "-", "--pk=id"],
+        {
+          input: JSON.stringify(tags),
+        }
+      );
     }
   } catch (error) {
     console.log(error);
@@ -62,6 +83,9 @@ needle.get(
 const postProcess = () => {
   spawnSync("sqlite-utils", ["enable-fts", "kinopio.db", "cards", "name"]);
   spawnSync("sqlite-utils", ["enable-fts", "kinopio.db", "spaces", "name"]);
+  spawnSync("sqlite-utils", ["enable-fts", "kinopio.db", "connections", "name"]);
+  spawnSync("sqlite-utils", ["enable-fts", "kinopio.db", "connectionTypes", "name"]);
+  spawnSync("sqlite-utils", ["enable-fts", "kinopio.db", "tags", "name"]);
   spawnSync("sqlite-utils", [
     "add-foreign-keys",
     "kinopio.db",
