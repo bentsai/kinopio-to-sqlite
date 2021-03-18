8b977c3a19acafde446536661fc7140e3679aeaa
Ben Tsai <bentsai@gmail.com>
2021-03-17 22:45:04 -0400

    Add plugin to linkify space URL

---
+++ plugins/link-space-url.py
@@ -0,0 +1,13 @@
+from datasette import hookimpl
+import jinja2
+
+@hookimpl
+def render_cell(value, column):
+    if column != "url":
+        return None
+    if not isinstance(value, str):
+        return None
+    stripped = value.strip()
+    return jinja2.Markup('<a href="{href}">→</a>'.format(
+        href=jinja2.escape("https://kinopio.club/" + stripped)
+    ))
